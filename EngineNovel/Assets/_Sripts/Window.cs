8615fcad87f8c567ea237a9f75f30bd211fe9e87
kotebot <kotebot1@gmail.com>
2018-10-17 18:54:25 +0300

    Base Engine

---
+++ EngineNovel/Assets/_Sripts/Window.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Window : EditorWindow {
+
+    public string branch = "main";
+
+    List<string> replics=new List<string>();
+    List<bool> act = new List<bool>();
+
+    [MenuItem("Sherman/Novel")]
+    public static void ShowWindow()
+    {
+        GetWindow<Window>("Example");
+    }
+
+    private void OnGUI()
+    {
+        act.Add(true);
+        GUILayout.Label("Novel", EditorStyles.boldLabel);
+
+        for (int i = 0, j = 0; i < replics.Count; i++)
+        { 
+            act[j] = GUILayout.Toggle(act[j], "Active");
+            branch = EditorGUILayout.TextField("Branch: ", branch);
+            if(act[j])
+            { 
+                for (;i<replics.Count;j++)
+                {         
+                    EditorGUILayout.TextField("Replic", replics[i]);
+                }
+                if(GUILayout.Button("+",EditorStyles.miniButtonRight))
+                {
+                    replics.Add("");
+                }
+                if (GUILayout.Button("-", EditorStyles.miniButtonRight))
+                {
+                    if(replics.Count!=0)
+                        replics.RemoveAt(replics.Count-1);
+                }
+            }
+        }
+    }
+}
